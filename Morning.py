0eb214b795ab586ce694f18c9abb4039ed3cb21f
Shy <shyamal@ruparel.co>
2016-12-23 17:53:39 -0500

    Modified path for img test.

--- Morning.py
+++ Morning.py
@@ -3,7 +3,7 @@ import Image
 
 printer = Adafruit_Thermal("/dev/ttyAMA0", 19200, timeout=5)
 
-im = Image.open("/img/GOODMORNING.png")
+im = Image.open("img/GOODMORNING.png")
 im.rotate(90)
 
 printer.printImage(im,FALSE)
