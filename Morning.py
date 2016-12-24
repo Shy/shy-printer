82a541c7db4583b5884de71e7813ff6464e09d97
Shy <shyamal@ruparel.co>
2016-12-23 20:35:49 -0500

    Updated image scripts.

--- Morning.py
+++ Morning.py
@@ -1,13 +1,17 @@
 from Adafruit_Thermal import *
 
-import Image
+from PIL import Image
 
 printer = Adafruit_Thermal("/dev/ttyAMA0", 19200, timeout=5)
 
 im = Image.open("assets/GOODMORNING.png")
-im = im.rotate(90)
 
-printer.printImage(im,False)
+new_width  = 384
+new_height = new_width * im.height / im.width
+
+im = im.resize((new_width,new_height))
+
+printer.printImage(im,True)
 
 im.save('assets/GOODMORNING.bmp')
 
