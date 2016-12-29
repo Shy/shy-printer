8250db82877cfa14946aef3530f6ea234db928ac
Shy <shyamal@ruparel.co>
2016-12-29 00:07:34 -0500

    Modified resize behavior.

--- Morning.py
+++ Morning.py
@@ -9,10 +9,11 @@ im = Image.open(sys.argv[1])
 
 width, height = im.size
 
-new_width  = 384
-new_height = new_width * height / width
-
-im = im.resize((new_width,new_height))
+#only resize if to big.
+if width > 384:
+    new_width  = 384
+    new_height = new_width * height / width
+    im = im.resize((new_width,new_height))
 
 printer.printImage(im,True)
 
